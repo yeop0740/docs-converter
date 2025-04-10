7a3c6b584acdac89d49c56b3a5763bdbf592d0f4
heeyeop <ssucgoo@gmail.com>
2025-04-10 22:34:04 +0900

    :closed_lock_with_key: 환경변수 설정

--- test-convert.py
+++ test-convert.py
@@ -3,15 +3,23 @@ import boto3
 import io
 import uuid
 from boto3.session import Session
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+profile_name = os.getenv("AWS_PROFILE")
+region_name = os.getenv("AWS_REGION")
+bucket_name = os.getenv("AWS_BUCKET_NAME")
 
 md = MarkItDown(enable_plugins=False)
 result = md.convert("로그 설정.pdf")
 tmp_file = io.BytesIO(result.text_content.encode('utf-8'))
 
-session = Session(profile_name='profile')
-s3 = boto3.client('s3', region_name = 'ap-northeast-2')
-
-bucket_name = 'bucket'
+session = Session(profile_name=profile_name)
+s3 = boto3.client('s3', region_name=region_name)
 key = str(uuid.uuid4())
 
 s3.upload_fileobj(tmp_file, bucket_name, key)
+
+print(f"s3://{bucket_name}/{key} 업로드 완료")
